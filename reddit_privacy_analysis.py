2f03e2796c76a419df8b91759a428ee9eff9f361
stormed-jupiter <113480903+stormed-jupiter@users.noreply.github.com>
2022-11-06 14:19:02 -0500

    Update reddit_privacy_analysis.py
    Added basic functionality to pull the submission datasets

--- reddit_privacy_analysis.py
+++ reddit_privacy_analysis.py
@@ -1,5 +1,16 @@
 import pandas as pd
-import praw
+import reddit_data
 
+# This line looks for a praw.ini config file in your working directory; See the config section of the readme for details
 reddit = praw.Reddit()
-print(reddit.user.me())
+
+# Define the subreddits that this analysis will target - CHANGE HERE FOR DIFFERENT SUBREDDITS
+target_subreddits = ['androiddev', 'webdev', 'iosdev']
+
+# Boolean to control if data is written to disk
+write_data_to_disk = True
+
+# Get all submissions for the target subreddits
+submissions = {}
+for target_subreddit in target_subreddits:
+  submissions[target_subreddit] = reddit_data.subreddit_submissions(reddit, target_subreddit, write_data_to_disk=write_data_to_disk)

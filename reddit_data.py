fcec13c8a735aa0dbc1a2e0df61fea6eedeff312
stormed-jupiter <113480903+stormed-jupiter@users.noreply.github.com>
2022-11-07 22:21:58 -0500

    Updated to do word frequency analysis

--- reddit_data.py
+++ reddit_data.py
@@ -4,30 +4,23 @@ import pandas as pd
 import praw
 from pmaw import PushshiftAPI
 
-reddit = praw.Reddit()
-api_praw = PushshiftAPI(praw=reddit)
-print('Connected as: %s' % reddit.user.me())
 
-# TODO Decide how to best handle the date parameters, probably as an input to the submission/comment functions
-before = int(datetime.datetime(2022, 11, 1, 0, 0).timestamp())
-after = int(datetime.datetime(2009, 7, 12, 0, 0).timestamp())
-
-
-def subreddit_submissions(reddit_instance: praw.Reddit, target_subreddit: str, submission_pull_limit: int = None,
-                          write_data_to_disk=True):
-    target_subreddit = "androiddev"
+def subreddit_submissions(api_instance: PushshiftAPI, target_subreddit: str,
+                          before: int = None, after: int = None, submission_pull_limit: int = None,
+                          write_data_to_disk: bool = True):
 
     # TODO Make a more formal method for timing and outputting progress
     start = time.perf_counter()
 
-    submissions = api_praw.search_submissions(subreddit=target_subreddit, limit=submission_pull_limit,
-                                              before=before, after=after)
+    # TODO Confirm behavior if before/after is None. There might be side-effects
+    submissions = api_instance.search_submissions(subreddit=target_subreddit, limit=submission_pull_limit,
+                                                  before=before, after=after)
     df_submissions = pd.DataFrame(submissions)
     # TODO Make a more formal method for timing and outputting progress
     print('Time to pull submissions: %f' % (time.perf_counter() - start))
     
     if write_data_to_disk:
         # TODO implement a smart way to supply a desired path here
-        df_submissions.to_csv('%s_submissions_raw_data.zip' % target_subreddit)
+        df_submissions.to_csv('Data\%s_submissions_raw_data.zip' % target_subreddit)
     
     return df_submissions
--- reddit_privacy_analysis.py
+++ reddit_privacy_analysis.py
@@ -1,16 +1,92 @@
 import pandas as pd
+import numpy as np
+import json
 import reddit_data
+from typing import Union
+from pmaw import PushshiftAPI
+import datetime
 
 # This line looks for a praw.ini config file in your working directory; See the config section of the readme for details
 reddit = praw.Reddit()
+api_praw = PushshiftAPI(praw=reddit)
+print('Connected as: %s' % reddit.user.me())
 
-# Define the subreddits that this analysis will target - CHANGE HERE FOR DIFFERENT SUBREDDITS
-target_subreddits = ['androiddev', 'webdev', 'iosdev']
+# TODO - Make config json or other solution to move selection options and controls outside code
+
+# Define the subreddits and dates that this analysis will target
+before = int(datetime.datetime(2022, 11, 1, 0, 0).timestamp())
+target_subreddits = [{'subreddit': 'androiddev',
+                      'before': before,
+                      'after': int(datetime.datetime(2009, 7, 12, 0, 0).timestamp()),
+                      'use_existing_data': True,
+                      'data_path': r'Data/androiddev_submissions_raw_data.zip'},
+                     {'subreddit': 'webdev',
+                      'before': before,
+                      'after': int(datetime.datetime(2009, 1, 25, 0, 0).timestamp()),
+                      'use_existing_data': True,
+                      'data_path': r'Data/webdev_submissions_raw_data.zip'},
+                     {'subreddit': 'iosdev',
+                      'before': before,
+                      'after': int(datetime.datetime(2010, 10, 13, 0, 0).timestamp()),
+                      'use_existing_data': True,
+                      'data_path': r'Data/iosdev_submissions_raw_data.zip'}]
 
 # Boolean to control if data is written to disk
 write_data_to_disk = True
 
+# Flag to use existing datasets or to pull new ones
+use_existing_data = True
+
 # Get all submissions for the target subreddits
 submissions = {}
 for target_subreddit in target_subreddits:
-  submissions[target_subreddit] = reddit_data.subreddit_submissions(reddit, target_subreddit, write_data_to_disk=write_data_to_disk)
+    if target_subreddit['use_existing_data']:
+        submissions[target_subreddit['target_subreddit']] = pd.read_csv(target_subreddit['data_path'])
+    else:
+        # TODO - Add functionality for use_existing_data = False case
+        pass
+
+
+def term_frequency(text: Union[pd.DataFrame, np.ndarray], target_words: list[str], target_columns=None):
+    if target_columns is None:
+        if type(text) == pd.DataFrame:
+            target_columns = text.columns
+        elif type(text) == np.Array:
+            target_columns = list(range(text.shape[1]))
+
+    results = []
+    row_count = text.shape[0]
+    for target_word in target_words:
+        for target_column in target_columns:
+            number_of_occurrences = np.sum(text[target_column].str.contains(target_word, case=False))
+            results.append({'target_word': target_word, 'target_column': target_column,
+                            'number_of_occurrences': number_of_occurrences, 'total_number_of_rows': row_count})
+
+    return results
+
+
+def privacy_keywords(text: Union[pd.DataFrame, np.ndarray], subreddit: str, reddit_data_type, target_columns=None):
+    # TODO - Refactor. This does not feel like it is implemented cleanly
+    with open(r'Config/analysis_settings.json') as json_file:
+        settings = json.load(json_file)
+    privacy_keywords = settings['privacy_keywords']
+
+    keywords_df = pd.DataFrame(term_frequency(text, privacy_keywords, target_columns=target_columns))
+    if reddit_data_type == 'submissions':
+        keywords_df.rename(columns={'target_word': 'Privacy Keyword',
+                                    'target_column': 'Search Location',
+                                    'number_of_occurrences': 'Number of Occurrences',
+                                    'total_number_of_rows': 'Number of Submissions Searched'},
+                           inplace=True)
+    else:
+        # TODO - Implement for comments
+        pass
+
+    if write_data_to_disk:
+        keywords_df.to_csv(r'Outputs\%s_%s_privacy_keywords.csv' % (subreddit, reddit_data_type))
+
+    return keywords_df
+
+def word_cloud(text, words_to_exclude):
+    pass
+
